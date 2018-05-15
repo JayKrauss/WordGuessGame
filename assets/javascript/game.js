f6b6fd8adfee3b12fc36eea2a014ea1c3d6b8558
Jay Krauss <paxformatus@gmail.com>
2018-05-15 19:51:01 -0400

    element comment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,43 @@
 //An array containing the words to be chosen from.
-var wordBank = ('horse')
-var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+var wordBank = ('horse', 'cattle', 'revolver', 'cowboy', 'lariat', 'horseshoe');
+
+var lives = 6;
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+
+//var horse = ('h', 'o', 'r', 's', 'e');
+
+//var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 document.onkeyup = function(event) {
     userText.textContent = event.key;
 }
-
 var pressedKey = event.key;
 
+//function getLives(guesses) {
+//    if (lives === 6) {
+//        document.getElementById('lives').innerHTML = '♥ ♥ ♥ ♥ ♥ ♥';
+//   }
+//    else if (lives === 5) {
+//        document.getElementById('lives').innerHTML = '♥ ♥ ♥ ♥ ♥';
+//    }
+//    else if (lives === 4) {
+//        document.getElementById('lives').innerHTML = '♥ ♥ ♥ ♥';
+//    }
+//    else if (lives === 3) {
+//        document.getElementById('lives').innerHTML = '♥ ♥ ♥';
+//    }
+//    else if (lives === 2) {
+//        document.getElementById('lives').innerHTML = '♥ ♥';
+//    }
+//    else if (lives === 1) {
+//        document.getElementById('lives').innerHTML = '♥';
+//    }
+//    else if (lives === 0) {
+//        document.getElementById('lives').innerHTML = 'GAME OVER';
+//    }
+//}
 //Function called by pressing Start button
 function gameStart() { 
 //Random generation of word choice.
@@ -15,7 +45,7 @@ function gameStart() {
 var randWord = 'horse'
 //Begin word logic
 
-//Horse Logic
+//Horse logic testing
 if (randWord === 'horse') {
     document.getElementById("slot1").innerHTML = '_';
     document.getElementById("slot2").innerHTML = '_';
@@ -27,6 +57,7 @@ if (randWord === 'horse') {
     document.getElementById("slot8").innerHTML = '';
     }
 }
+
 function checkKeyPress(key){
     if (key.keyCode == '72'){
         document.getElementById("slot1").innerHTML = 'H';
@@ -44,7 +75,8 @@ function checkKeyPress(key){
         document.getElementById("slot5").innerHTML = 'E';
     }
     else {
-       
+       lives = lives - 1;
     }
 }
+
 
