609a9dde618c0edea00801357edb6a753b95874e
Jay <paxformatus@gmail.com>
2018-05-15 00:01:24 -0400

    JS Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,32 @@
 //An array containing the words to be chosen from.
-var wordBank = ('horse', 'saddle', 'cowboy', 'cactus', 'cattle', 'revolver', 'railroad', 'desert', 'lariat', 'hat')
+var wordBank = ('horse')
 var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
+document.onkeyup = function(event) {
+    userText.textContent = event.key;
+}
 
+var pressedKey = event.key;
 
+//Function called by pressing Start button
 function gameStart() { 
-    alert(randWord);
+//Random generation of word choice.
+//var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+var randWord = 'horse'
+//Begin word logic
+if (randWord === 'horse') {
+    document.getElementById("slot1").innerHTML = '_';
+    document.getElementById("slot2").innerHTML = '_';
+    document.getElementById("slot3").innerHTML = '_';
+    document.getElementById("slot4").innerHTML = '_';
+    document.getElementById("slot5").innerHTML = '_';
+    document.getElementById("slot6").innerHTML = '';
+    document.getElementById("slot7").innerHTML = '';
+    document.getElementById("slot8").innerHTML = '';
+    }
 
+if (pressedKey === 'h'){
+   document.getElementById("slot1").innerHTML = 'H';
+        
+            }
 }
