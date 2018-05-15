c0332d40dea34217d5fee4c486c6a96c0931f7d1
Jay Krauss <paxformatus@gmail.com>
2018-05-14 20:13:55 -0400

    Third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,10 @@
 //An array containing the words to be chosen from.
 var wordBank = ('horse', 'saddle', 'cowboy', 'cactus', 'cattle', 'revolver', 'railroad', 'desert', 'lariat', 'hat')
+var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
+
+
+function gameStart() { 
+    alert(randWord);
+
+}
